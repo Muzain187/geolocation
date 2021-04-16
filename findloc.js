dd8fe4b9ae44a24c3335c38605fa4d071653ebb9
Muzain187 <60264379+Muzain187@users.noreply.github.com>
2021-04-16 16:57:26 +0530

    Update findloc.js

--- findloc.js
+++ findloc.js
@@ -1,11 +1,36 @@
 let button = document.getElementById('Btn');
 let display = document.getElementById('print');
-let loc;
+let qibla = document.getElementById('qibla');
+let lat,lon;
+const con = Math.PI/180;
+let Mlat = 21.25,Mlon = 39.49;
+let Dlat,Dlon;
 button.addEventListener('click',function(){
     console.log("hii");
-    navigator.geolocation.getCurrentPosition(pos =>  loc = pos.coords);
-    console.log(loc);
-    console.log('latitude',loc.latitude,'\nlongitude',loc.longitude);
-    display.innerHTML = `latitude : ${loc.latitude}<br>longitude : ${loc.longitude}`;
+    navigator.geolocation.getCurrentPosition(pos =>  {
+        lat = pos.coords.latitude;
+        lon = pos.coords.longitude;
+    });
+    // console.log(loc);
+    if(typeof(lat) != 'undefined' && typeof(lon) != 'undefined'){
+        console.log('latitude',lat,'\nlongitude',lon);
+        display.innerHTML = `latitude : ${lat}<br>longitude : ${lon}`;
+
+        Dlat = lat;
+        Dlon = lon;
+        let A = Math.sin((Dlon*con)-(Mlon*con));
+        console.log(A);
+        let B = Math.cos(Dlat*con)*Math.tan(Mlat*con)-Math.sin(Dlat*con)*Math.cos((Dlon*con)-(Mlon*con));
+        console.log(B);
+        let C = A/B;
+        let D = Math.atan(C)*180/Math.PI;
+        let direction  = Math.round(360-D);
+        qibla.innerHTML = `The Qibla direction angle is ${direction}`;
+         console.log(360-D);
+    }
+    else{
+        qibla.innerHTML = `Please re click the button until the result show`;
+    }
+
     
 })
